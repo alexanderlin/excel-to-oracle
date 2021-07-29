000910c418107d5c269632dfef85388d0f51d4fc
alexanderlin <alexraptordev@gmail.com>
2021-07-29 17:12:39 -0400

    can access and modify oracle db. rust program must be in windows not wsl 2 or oracle db wouldn't be found

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::env;
-use calamine::{open_workbook, Error, Xlsx, Reader, RangeDeserializerBuilder};
+use calamine::{open_workbook, Xlsx, Reader};
+use oracle::{Connection, Result};
 
 pub fn read_file(){
     let args: Vec<String> = env::args().collect();
@@ -9,10 +10,20 @@ pub fn read_file(){
 
     let mut excel: Xlsx<_> = open_workbook(file).unwrap();
     if let Some(Ok(r)) = excel.worksheet_range("Sheet1"){
-        // for row in r.rows(){
-        //     println!("row={:?}, row[0]={:?}", row, row[0]);
-        // }
-        println!("row={:?}, row[0]={:?}", r.row, r.row[0]);
+        for row in r.rows(){
+            //println!("row={:?}, row[0]={:?}", row, row[0]);
+            for r_item in row {
+                println!("row item: {:?}",r_item);
+            }
+        }
+        
 
     }
+}
+
+pub fn connect_to_oracle()->Result<String>{
+    let conn = Connection::connect("test", "test", "localhost/xe")?;
+    conn.execute("create table person (id number(38), name varchar2(40))", &[])?;
+    conn.commit()?;
+    Ok("Success!".to_string())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use excel_to_oracle::read_file;
+use excel_to_oracle::{read_file, connect_to_oracle};
 
 fn main() {
     read_file();
+    println!("now connecting to oracle database");
+    println!("{:?}",connect_to_oracle());
 }
