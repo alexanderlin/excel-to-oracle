371a8323fe9872588985c604fc25e69717e5f22e
alexanderlin <alexraptordev@gmail.com>
2021-07-31 15:22:33 -0400

    only column names will have special characters removed

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,12 @@ pub fn read_write()->Vec<Result<String>> {
     let args: Vec<String> = env::args().collect();
 
     let file = &args[1];
+    let column_capacity = &args[2];
+    //info for database username, password, location
+    let mut db_info = Vec::new();
+    db_info.push(&args[3]);
+    db_info.push(&args[4]);
+    db_info.push(&args[5]);
     println!("In file {}", file);
 
     let mut excel: Xlsx<_> = open_workbook(file).unwrap();
@@ -14,10 +20,13 @@ pub fn read_write()->Vec<Result<String>> {
     let sheets = excel.sheet_names().to_owned();
     let mut sheets_data = Vec::new();
     for s in &sheets {
+        //flag to make sure only first row has special characters changed
+        let mut f = 0;
         let mut data_by_row = Vec::new();
         if let Some(Ok(r)) = excel.worksheet_range(s){
             for row in r.rows(){
                 let mut row_holder= Vec::new();
+                if f == 0{ 
                 for r_item in row {
                     match r_item {
                         DataType::String(x) => {
@@ -29,6 +38,18 @@ pub fn read_write()->Vec<Result<String>> {
     
                     }
                 }
+                f = f + 1;
+            }
+            else{
+                for r_item in row {
+                    match r_item {
+                        DataType::String(x) => row_holder.push(x.to_string()),
+                        _ => row_holder.push("".to_string()),
+    
+                    }
+                }
+            }
+
                 data_by_row.push(row_holder);
             }
             //println!("{:?}",data_by_row);
@@ -37,28 +58,27 @@ pub fn read_write()->Vec<Result<String>> {
 
     }
     for (i,s) in sheets.iter().enumerate(){
-        result.push(write_to_oracle(s, &mut sheets_data[i]));
+        result.push(write_to_oracle(s, &mut sheets_data[i], column_capacity, &db_info));
     }
     result
 }
 
-pub fn write_to_oracle(sheet_name: &str, data_by_row: &mut Vec<Vec<String>>)->Result<String>{
-    let conn = Connection::connect("test", "test", "localhost/xe")?;
+pub fn write_to_oracle(sheet_name: &str, data_by_row: &mut Vec<Vec<String>>, column_capacity: &str, db_info: &Vec<&String>)->Result<String>{
+    let conn = Connection::connect(db_info[0],db_info[1],db_info[2])?;
 
     //create the table
     let mut sql = format!("create table {} ( ",sheet_name);
     for (i,data) in data_by_row[0].clone().iter().enumerate(){
         sql.push_str(&data);
         if i+1 >= data_by_row[0].len(){
-            sql.push_str(" varchar2(1000) ");
+            sql.push_str(&format!(" varchar2({}) ", column_capacity));
         }
         else{
-            sql.push_str(" varchar2(1000), ");
+            sql.push_str(&format!(" varchar2({}), ", column_capacity));
         }
     }
     sql.push_str(" ) ");
 
-    println!("{}",sql);
     conn.execute(&sql, &[])?;
     conn.commit()?;
 
@@ -75,7 +95,6 @@ pub fn write_to_oracle(sheet_name: &str, data_by_row: &mut Vec<Vec<String>>)->Re
         }
 
     }
-    println!("insert statement: {}",sql2);
     let mut stmt = conn.prepare(&sql2,&[])?;
     for row in data_by_row {
         for (i,ri) in row.iter().enumerate() {
